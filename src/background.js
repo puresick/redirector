8a5166c36768be8ef9ce76b221b4e1364645cdad
puresick <2714266+puresick@users.noreply.github.com>
2020-03-18 16:10:39 +0100

    Add support for dark mode and light/dark custom themes

--- src/background.js
+++ src/background.js
@@ -1,22 +1,46 @@
-browser.browserAction.onClicked.addListener(async event => {
-	const { active } = await browser.storage.local.get()
-	const newState = !active
-	const path = newState
-		? "assets/icon-48-normal.png"
-		: "assets/icon-48-disabled.png"
-	const log = newState
-		? "enabled"
-		: "disabled"
-
-	await browser.storage.local.set({ active: newState })
-	browser.browserAction.setIcon({ path })
-	console.log(log)
-})
+const iconPaths = {
+	light: "assets/icon-48-light.png",
+	dark: "assets/icon-48-dark.png",
+	disabled: "assets/icon-48-disabled.png"
+}
 
 browser.runtime.onInstalled.addListener(async event => {
 	const active = true
 
-	await browser.storage.local.set({
-		active
-	})
+	await browser.storage.local.set({ active })
 })
+
+browser.runtime.onStartup.addListener(async event => {
+	const pathDefaultIcon = handleToolbarIcon()
+
+	browser.browserAction.setIcon({ path })
+})
+
+browser.theme.onUpdated.addListener(async event => {
+	const browserInfo = await browser.theme.getCurrent()
+	const { icons } = browserInfo.colors
+	const rgbValue = parseInt(icons.split(",")[1])
+	const pathDefaultIcon = (rgbValue > 150)
+		? iconPaths.light
+		: iconPaths.dark
+
+	browser.browserAction.setIcon({ path })
+})
+
+browser.browserAction.onClicked.addListener(async event => {
+	const pathDefaultIcon = handleToolbarIcon()
+	const { active } = await browser.storage.local.get()
+	const newState = !active
+	const path = newState
+		? pathDefaultIcon
+		: iconPaths.disabled
+
+	await browser.storage.local.set({ active: newState })
+	browser.browserAction.setIcon({ path })
+})
+
+const handleToolbarIcon = () => {
+	return (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches)
+		? iconPaths.light
+		: iconPaths.dark
+}
